3c809cdd2c9c1e13a595a450eb6b09ede8c3e7e4
BabaYettu <62243016+BabaYettu@users.noreply.github.com>
2020-03-17 19:59:45 +0100

    Add files via upload

--- pic_perso.js
+++ pic_perso.js
@@ -7,22 +7,34 @@ function changerCouleur(){
 	$("#couleurChoisie").css("background-color", couleur);
 }	
 
-function faire(){
-	console.log("faire");
-	if (clic){
-		dessiner();
-		setTimeout(faire, 1000);
+// function faire(){
+	// console.log("faire");
+	// if (clic){
+		// dessiner();
+		// setTimeout(faire, 2000);
+	// }
+	// else{
+		// return false;
+	// }
+// }
+
+var ev;
+
+function w(){
+	$("#canvas").mousemove(function(e){
+	ev = e;
+	dessiner();
 	}
-	else{
-		return ;
-	}
-}
+);}
+
+$("#canvas").mousedown(w);
+
 
 function dessiner(){
 	console.log(ev);
 	var X = ev.pageX - $("#canvas").offset().left;
 	var Y = ev.pageY - $("#canvas").offset().top;
-	ctx.fillStyle = $("couleurChoisie").css("background-color");
+	ctx.fillStyle = $("#couleurChoisie").css("background-color");
 	ctx.lineWidth = 8;
 	ctx.beginPath();
 	ctx.arc(X, Y, 3, 0, 2 *Math.PI);
@@ -30,17 +42,13 @@ function dessiner(){
 }
 
 
-var clic = false;
-var ev;
+
 
 
 $("#palette td").click(changerCouleur);
 
-$("#canvas").click(function(e){
-	clic = true;
-	ev = e;
-	faire();
-	});
+
+
 /*$("#canvas").click(function(e){
 	f(e);
 	});*/
