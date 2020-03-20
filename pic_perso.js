40bb08c41ac967845518754e2aae5bf17e82eaaf
Darkman-dev <62433298+Darkman-dev@users.noreply.github.com>
2020-03-20 16:32:47 +0100

    Add files via upload

--- pic_perso.js
+++ pic_perso.js
@@ -9,42 +9,51 @@ function changerCouleur(){
 
 $("#palette td").click(changerCouleur);
 
-let isDrawing = false;
-let x = 0;
-let y = 0;
+$("#trait").click(function(){
+	let isDrawing = false;
+	let x = 0;
+	let y = 0;
 
-const rect = canvas.getBoundingClientRect();
+	const rect = canvas.getBoundingClientRect();
 
-canvas.addEventListener('mousedown', e => {
-  x = e.clientX - rect.left;
-  y = e.clientY - rect.top;
-  isDrawing = true;
+	canvas.addEventListener('mousedown', e => {
+	  x = e.clientX - rect.left;
+	  y = e.clientY - rect.top;
+	  isDrawing = true;
+	});
+
+	canvas.addEventListener('mousemove', e => {
+	  if (isDrawing === true) {
+		dessiner(ctx, x, y, e.clientX - rect.left, e.clientY - rect.top);
+		x = e.clientX - rect.left;
+		y = e.clientY - rect.top;
+	  }
+	});
+
+	window.addEventListener('mouseup', e => {
+	  if (isDrawing === true) {
+		dessiner(ctx, x, y, e.clientX - rect.left, e.clientY - rect.top);
+		x = 0;
+		y = 0;
+		isDrawing = false;
+	  }
+	});
+
+	function dessiner(ctx, x1, y1, x2, y2) {
+	  ctx.beginPath();
+	  ctx.strokeStyle = $("#couleurChoisie").css("background-color");
+	  ctx.lineWidth = 5;
+	  ctx.moveTo(x1, y1);
+	  ctx.lineTo(x2, y2);
+	  ctx.joinLine = "round";
+	  ctx.lineCap = "round";
+	  ctx.stroke();	 
+	  ctx.closePath();
+	}
 });
 
-canvas.addEventListener('mousemove', e => {
-  if (isDrawing === true) {
-	dessiner(ctx, x, y, e.clientX - rect.left, e.clientY - rect.top);
-	x = e.clientX - rect.left;
-	y = e.clientY - rect.top;
-  }
-});
 
-window.addEventListener('mouseup', e => {
-  if (isDrawing === true) {
-	dessiner(ctx, x, y, e.clientX - rect.left, e.clientY - rect.top);
-	x = 0;
-	y = 0;
-	isDrawing = false;
-  }
-});
 
-function dessiner(ctx, x1, y1, x2, y2) {
-  ctx.beginPath();
-  ctx.strokeStyle = $("#couleurChoisie").css("background-color");
-  ctx.lineWidth = 5;
-  ctx.moveTo(x1, y1);
-  ctx.lineTo(x2, y2);
-  ctx.stroke();
-  ctx.joinLine = "round";
-  ctx.closePath();
-}
+$("#gomme").click(function(){
+	
+});
